9f599a1785ecfa8964702589b1bebc0924ad480a
Gabriel Duarte <gabrieldrtsr@gmail.com>
2024-11-27 12:28:42 -0300

    Modified some variable names to be more representative of what they mean.

--- c-life/src/main.c
+++ c-life/src/main.c
@@ -6,25 +6,25 @@
 #include "board.h"
 #include "pool.h"
 
-void print_boards(board_t *b){
+void print_boards(board_t *board){
     // printf("board 0:\n");
-    for(int i = 0; i < b->sideLenght; i++){
-        for(int j = 0; j < b->sideLenght; j++){
-            if(b->boards[0][i*b->sideLenght + j]) printf("x");
+    for(int i = 0; i < board->sideLenght; i++){
+        for(int j = 0; j < board->sideLenght; j++){
+            if(board->boards[0][i*board->sideLenght + j]) printf("x");
             else printf("_");
         }
         printf("\n");
     }
 }
 
-int check_if_boards_different(board_t* b1, board_t* b2){
-    if(b1->sideLenght != b2->sideLenght){
+int check_if_boards_different(board_t* board1, board_t* board2){
+    if(board1->sideLenght != board2->sideLenght){
         return 1;
     }
 
     int d = 0;
-    for(int i = 0; i < b1->sideLenght*b1->sideLenght; i++){
-        d += (b1->boards[0][i] != b2->boards[0][i]);
+    for(int i = 0; i < board1->sideLenght*board1->sideLenght; i++){
+        d += (board1->boards[0][i] != board2->boards[0][i]);
     }
     return d;
 }
@@ -34,10 +34,10 @@ int main(void){
     int sideLength = 1<<10;
     int iterations = 1<<9;
     int nWorkers = 8;
-    double p = 0.5;
+    double probability = 0.5;
 
-    board_t* b1 = create_board(sideLength);
-    board_t* b2 = create_board(sideLength);
+    board_t* board1 = create_board(sideLength);
+    board_t* board2 = create_board(sideLength);
 
     int alive = 0;
 
@@ -47,45 +47,45 @@ int main(void){
     srand(time(NULL));
     //lets populate randomly manually
     for(int i = 0; i < sideLength*sideLength; i++){
-        b1->boards[0][i] =  ((((double) rand())/RAND_MAX) <= p);
-        b2->boards[0][i] = b1->boards[0][i];
-        alive += b1->boards[0][i];
+        board1->boards[0][i] =  ((((double) rand())/RAND_MAX) <= probability);
+        board2->boards[0][i] = board1->boards[0][i];
+        alive += board1->boards[0][i];
     }
 
-    printf("diffs ? : %d\n",check_if_boards_different(b1,b2));
+    printf("diffs ? : %d\n",check_if_boards_different(board1,board2));
     
-    struct timeval tv1, tv2;
+    struct timeval timeValue1, timeValue2;
         
 
     //concurrent application!
-    gettimeofday(&tv1,NULL);
-    pool_board* pb = create_pool(nWorkers,b2);
-    start_pool(pb);
+    gettimeofday(&timeValue1,NULL);
+    pool_board* poolBoard = create_pool(nWorkers,board2);
+    start_pool(poolBoard);
 
     for(int i = 0; i < iterations; i++){
-        iterate_concurrently(pb);
+        iterate_concurrently(poolBoard);
     }    
-    liberate_threads(pb);
-    gettimeofday(&tv2,NULL);
+    liberate_threads(poolBoard);
+    gettimeofday(&timeValue2,NULL);
     printf ("Concurrent program elapsed: %lfs\n",
-        (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-        (double) (tv2.tv_sec - tv1.tv_sec));
+        (double) (timeValue2.tv_usec - timeValue1.tv_usec) / 1000000 +
+        (double) (timeValue2.tv_sec - timeValue1.tv_sec));
     
     // sequential application!
-    gettimeofday(&tv1,NULL);
+    gettimeofday(&timeValue1,NULL);
     for(int i = 0; i < iterations; i++){
-        iterate_sequentially(b1);
+        iterate_sequentially(board1);
     }
-    gettimeofday(&tv2,NULL);
+    gettimeofday(&timeValue2,NULL);
 
     printf ("Sequential program elapsed: %lfs\n",
-        (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-        (double) (tv2.tv_sec - tv1.tv_sec));
+        (double) (timeValue2.tv_usec - timeValue1.tv_usec) / 1000000 +
+        (double) (timeValue2.tv_sec - timeValue1.tv_sec));
     
     //freeing space
-    destroy_board(b1);
-    destroy_board(b2);
-    destroy_pool(pb);
+    destroy_board(board1);
+    destroy_board(board2);
+    destroy_pool(poolBoard);
 
     return 0;
 }
